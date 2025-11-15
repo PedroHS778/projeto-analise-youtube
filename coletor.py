52ddad3c66d8caa301058118e8d0a10789824677
PedroHS778 <peh4santos@gmail.com>
2025-11-15 18:32:45 -0300

    Atualização

--- coletor.py
+++ coletor.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 
 # --- Configurações ---
 # ID do vídeo
-VIDEO_ID = "BYy278lrtNA" 
+VIDEO_ID = "i2tFPyw6Iqg" 
 
 # Nome do banco de dados
 DB_NAME = "youtube_stats.db"
