11bc73bfb3cfcecd031fb3cb07f52b488b4731fa
PedroHS778 <peh4santos@gmail.com>
2025-11-15 19:15:57 -0300

    Troca o ID do vídeo e reseta o banco de dados

--- coletor.py
+++ coletor.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 
 # --- Configurações ---
 # ID do vídeo
-VIDEO_ID = "8uaQnl9Bbdw" 
+VIDEO_ID = "BYy278lrtNA" 
 
 # Nome do banco de dados
 DB_NAME = "youtube_stats.db"
